01c4ea07d29d3e255af8055f39645e968ebc304c
tonethar <1085060+tonethar@users.noreply.github.com>
2024-05-03 09:37:51 -0400

    Committed FlowerPetalDrawFunc.js

---
+++ src/types/FlowerPetalDrawFunc.js
@@ -0,0 +1,15 @@
+// @ts-check
+/**
+ * @name FlowerPetalDrawFunc
+ * @desc JSDoc type definition
+ * @author TJ
+ * @typedef {Object} FlowerPetalDrawFunc
+ * @prop {CanvasRenderingContext2D} ctx - drawing context where the petal will be rendered.
+ * @prop {number} x - 'x' of petal.
+ * @prop {number} y - 'y' of petal.
+ * @prop {number} radius - 1/2 width of each petal in pixels.
+ * @prop {string} color - CSS color of petal.
+ */
+
+
+//(ctx, x, y, radius, color)
